20f942d6c93bfdfc15113501aad3eb248f89eab3
Annmarie Rodriguez <55721341+AnnmarieJenny@users.noreply.github.com>
2021-03-18 21:48:33 -0400

    added layer toggles and popups

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ map.on('load', function(){
         data: 'data/long-beach.geojson'
     });
     map.addLayer({
-        'id': 'long-beach-fill',
+        'id': 'Long Beach City',
         'type': 'fill',
         'source': 'long-beach',
         'layout': {},
@@ -35,7 +35,7 @@ map.on('load', function(){
         data: 'data/zoning.geojson'
     });
     map.addLayer({
-        'id': 'zoning-fill',
+        'id': 'Land Uses',
         'type': 'fill',
         'source': 'zoning',
         'layout': {},
@@ -51,7 +51,7 @@ map.on('load', function(){
         data: 'data/pov-rate.geojson'
     });
     map.addLayer({
-        'id': 'pov-rate-fill',
+        'id': 'Poverty Rate (2016)',
         'type': 'fill',
         'source': 'pov-rate',
         'layout': {},
@@ -67,7 +67,7 @@ map.on('load', function(){
         data: 'data/flood-zones.geojson'
     });
     map.addLayer({
-        'id': 'flood-zones-fill',
+        'id': 'Flood Zones',
         'type': 'fill',
         'source': 'flood-zones',
         'layout': {},
@@ -83,7 +83,7 @@ map.on('load', function(){
         data: 'data/eq-impact.geojson'
     });
     map.addLayer({
-        'id': 'eq-impact-fill',
+        'id': 'Liquefaction Zone',
         'type': 'fill',
         'source': 'eq-impact',
         'layout': {},
@@ -92,4 +92,119 @@ map.on('load', function(){
           'fill-opacity': 0.5
         }
     });
+
+
+/* play around with legend after figuring out how to toggle
+  // add in ranges and colors
+  var layers = ['0-10', '10-20', '20-50', '50-100', '100-200', '200-500', '500-1000', '1000+'];
+  var colors = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'];
+  // add in a legend
+  for (i = 0; i < layers.length; i++) {
+    var layers = layers[i];
+    var color = colors[i];
+    var item = document.createElement('div');
+    var key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
+
+    var value = document.createElement('span');
+    value.innerHTML = layer;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+  }*/
+
+  //enumerate ids of the layers
+  var toggleableLayerIds = [
+    'Long Beach City', 'Land Uses', 'Poverty Rate (2016)', 'Flood Zones', 'Liquefaction Zone'];
+
+  // set up the corresponding toggle button for each layer
+  for (var i = 0; i < toggleableLayerIds.length; i++) {
+  var id = toggleableLayerIds[i];
+
+  var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+  link.onclick = function (e) {
+  var clickedLayer = this.textContent;
+    e.preventDefault();
+    e.stopPropagation();
+
+  var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+    // toggle layer visibility by changing the layout object's visibility property
+    if (visibility === 'visible') {
+        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+        this.className = '';
+    }
+    else {
+        this.className = 'active';
+        map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+    }
+    };
+
+  var layers = document.getElementById('menu');
+  layers.appendChild(link);
+  }
+
+  // create pop up with multiple properties listed below
+  map.on('click', function (e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['Long Beach City', 'Land Uses', 'Poverty Rate (2016)', 'Flood Zones', 'Liquefaction Zone']
+    });
+    // use function (VacantLots) from above to create a var that pulls from that data
+    var myHTML = `
+        <div><b>Name: </b>${features[0].properties.Name}</div>
+        `
+    new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML(myHTML)
+      .addTo(map);
+    });
+
+  // turn pointer on when it hovers over geos/vacant lots
+  map.on('mouseenter', 'Long Beach City', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+      })
+  // turn pointer off when it hovers away from geos/vacant lots
+  map.on('mouseleave', 'Long Beach City', (e) => {
+    map.getCanvas().style.cursor = '';
   })
+  // turn pointer on when it hovers over geos/vacant lots
+  map.on('mouseenter', 'Land Uses', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+      })
+  // turn pointer off when it hovers away from geos/vacant lots
+  map.on('mouseleave', 'Land Uses', (e) => {
+    map.getCanvas().style.cursor = '';
+  })
+
+  // turn pointer on when it hovers over geos/vacant lots
+  map.on('mouseenter', 'Poverty Rate (2016)', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+      })
+  // turn pointer off when it hovers away from geos/vacant lots
+  map.on('mouseleave', 'Poverty Rate (2016)', (e) => {
+    map.getCanvas().style.cursor = '';
+  })
+  // turn pointer on when it hovers over geos/vacant lots
+  map.on('mouseenter', 'Flood Zones', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+      })
+  // turn pointer off when it hovers away from geos/vacant lots
+  map.on('mouseleave', 'Flood Zones', (e) => {
+    map.getCanvas().style.cursor = '';
+  })
+
+  // turn pointer on when it hovers over geos/vacant lots
+  map.on('mouseenter', 'Liquefaction Zone', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+      })
+  // turn pointer off when it hovers away from geos/vacant lots
+  map.on('mouseleave', 'Liquefaction Zone', (e) => {
+    map.getCanvas().style.cursor = '';
+  })
+
+})
